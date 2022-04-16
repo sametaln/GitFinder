b1986109acbdde5f6a93dfe750792b12f402e066
sametaln <a.sametalan56@gmail.com>
2022-04-16 19:49:31 +0300

    updated styles for user page

--- src/components/repoItem/RepoItem.tsx
+++ src/components/repoItem/RepoItem.tsx
@@ -2,11 +2,23 @@ import './repoitem.scss';
 
 const RepoItem = ({ repo }: { repo: any }) => {
   return (
-    <div className="user-repo-item" key={repo.id}>
-      <h2 className="user-repo-item-name">{repo.name}</h2>
-      <p className="user-repo-item-description">
-        {repo.description ? repo.description : ''}
-      </p>
+    <div className="user-repo-item">
+      <div className="user-repo-item-left">
+        <h2 className="user-repo-item-name">{repo.name}</h2>
+        <p className="user-repo-item-description">
+          {repo.description ? repo.description : null}
+        </p>
+      </div>
+      <div className="user-repo-item-right">
+        <div className="user-repo-item-stats">
+          <p className="user-repo-item-stats-count" id="count">
+            {repo.stargazers_count}
+          </p>
+          <label htmlFor="count" className="user-repo-item-stats-count-label">
+            Stars
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -4,45 +4,71 @@ import Repos from '../../components/Repos/Repos';
 
 const User = (user: any) => {
   const [repos, setRepos] = useState<any[]>([]);
+  const [count, setCount] = useState(2);
   useEffect(() => {
     fetch(`https://api.github.com/users/${user.user.login}/repos`)
       .then((res) => res.json())
       .then((data) => {
         setRepos(data);
       });
-  }, []);
+  }, [user.user.login]);
 
   const handleClick = () => {
-    console.log('clicked');
+    if (count > repos.length) {
+      setCount(repos.length + 1);
+      return;
+    }
+    setCount(count + 2);
   };
 
   return (
     <div className="user-wrapper">
-      <div className="user-info">
-        <img
-          className="user-avatar"
-          src={user.user.avatar_url}
-          alt="User avatar"
-        />
-        <h2 className="user-name">{user.user.login}</h2>
-        <div className="user-stats">
-          <div className="user-stats-repos">{repos?.length}</div>
-          <div className="user-stats-following">
-            {user.user.following < 1000
-              ? user.user.following
-              : `${(user.user.following / 1000).toFixed(1)}k`}
+      <div className="user">
+        <div className="user-info">
+          <div className="user-info-img">
+            <img
+              className="user-info-avatar"
+              src={user.user.avatar_url}
+              alt="User avatar"
+            />
           </div>
-          <div className="user-stats-follower">
-            {user.user.followers < 1000
-              ? user.user.followers
-              : `${(user.user.followers / 1000).toFixed(1)}k`}
+          <div className="user-info-container">
+            {user.user.name ? (
+              <h1 className="user-info-name">{user.user.name}</h1>
+            ) : null}
+            <h2 className="user-info-login">@{user.user.login}</h2>
+            <div className="user-info-stats">
+              <div className="user-stats-repos">
+                <p id="length">{repos?.length}</p>
+                <label htmlFor="length">Repositories</label>
+              </div>
+              <div className="user-stats-following">
+                <p id="following">
+                  {user.user.following < 1000
+                    ? user.user.following
+                    : `${(user.user.following / 1000).toFixed(1)}k`}
+                </p>
+                <label htmlFor="following">Following</label>
+              </div>
+              <div className="user-stats-follower">
+                <p id="followers">
+                  {user.user.followers < 1000
+                    ? user.user.followers
+                    : `${(user.user.followers / 1000).toFixed(1)}k`}
+                </p>
+                <label htmlFor="followers">Followers</label>
+              </div>
+            </div>
           </div>
         </div>
+        <div className="repo-container">
+          <h1 className="repo-container-title">Repositories</h1>
+          <Repos repos={repos.slice(0, count)} />
+          <button className="user-load" onClick={handleClick}>
+            LOAD MORE
+          </button>
+        </div>
       </div>
-      <Repos repos={repos} />
-      <button className="user-load" onClick={handleClick}>
-        LOAD MORE
-      </button>
     </div>
   );
 };
