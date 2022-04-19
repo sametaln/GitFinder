93530f4cfc37b4e692d6be943ee73165f43c49bd
sametaln <a.sametalan56@gmail.com>
2022-04-19 14:09:12 +0300

    show less implemented

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -10,6 +10,7 @@ const User = (user: any, setError: any) => {
   const [repos, setRepos] = useState<any[]>([]);
   const [count, setCount] = useState(2);
   const [warning, setWarning] = useState('');
+  const [more, setMore] = useState(true);
 
   useEffect(() => {
     try {
@@ -23,13 +24,25 @@ const User = (user: any, setError: any) => {
     }
   }, [userObject.login]);
 
-  const handleClick = () => {
-    if (count > repos.length) {
-      setCount(repos.length + 1);
+  const handleClickMore = () => {
+    if (count === repos.length) {
+      setMore(false);
       setWarning('You have load all repositories');
+      setCount(repos.length + 1);
       return;
     }
     setCount(count + 2);
+  };
+
+  const handleClickLess = () => {
+    console.log(count);
+    if (count === 3) {
+      setWarning('');
+      setMore(true);
+      setCount(2);
+      return;
+    }
+    setCount(count - 2);
   };
 
   repos.sort(
@@ -42,8 +55,8 @@ const User = (user: any, setError: any) => {
 
   return (
     <div className="user-wrapper">
-      {warning && count == repos.length + 1 ? (
-        <div className="user-warning">
+      {warning && count === repos.length + 1 ? (
+        <div className="user-warning" onTransitionEnd={() => setWarning('')}>
           <p>{warning}</p>
         </div>
       ) : null}
@@ -53,9 +66,15 @@ const User = (user: any, setError: any) => {
           <h1 className="repo-container-title">Repositories</h1>
           <Repos repos={repos.slice(0, count)} />
           <div className="repo-container-buttons">
-            <button className="user-load more" onClick={handleClick}>
-              Load More
-            </button>
+            {more ? (
+              <button className="user-load more" onClick={handleClickMore}>
+                Load More
+              </button>
+            ) : (
+              <button className="user-load less" onClick={handleClickLess}>
+                Show Less
+              </button>
+            )}
             <Link className="user-load" to="/">
               Go Back
             </Link>
