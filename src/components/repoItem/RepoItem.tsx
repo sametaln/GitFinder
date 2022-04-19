b669d76d75a1d683d2433401cf0b382b0e15358e
sametaln <a.sametalan56@gmail.com>
2022-04-19 14:55:07 +0300

    style changes

--- src/components/repoItem/RepoItem.tsx
+++ src/components/repoItem/RepoItem.tsx
@@ -1,30 +1,38 @@
 import './repoitem.scss';
+import { useState } from 'react';
 
 const RepoItem = ({ repo }: { repo: any }) => {
   return (
-    <a
-      href={repo.html_url}
-      target="_blank"
-      rel="noreferrer"
-      className="user-repo-item"
-    >
-      <div className="user-repo-item-left">
-        <h2 className="user-repo-item-name">{repo.name}</h2>
-        <p className="user-repo-item-description">
-          {repo.description ? repo.description : null}
-        </p>
+    <>
+      <div className="user-repo-item">
+        <a
+          href={repo.html_url}
+          target="_blank"
+          rel="noreferrer"
+          className="user-repo-item-link"
+        >
+          <div className="user-repo-item-left">
+            <h2 className="user-repo-item-name">{repo.name}</h2>
+            <p className="user-repo-item-description">
+              {repo.description ? repo.description : null}
+            </p>
+          </div>
+          <div className="user-repo-item-right">
+            <div className="user-repo-item-stats">
+              <p className="user-repo-item-stats-count" id="count">
+                {repo.stargazers_count}
+              </p>
+              <label
+                htmlFor="count"
+                className="user-repo-item-stats-count-label"
+              >
+                Stars
+              </label>
+            </div>
+          </div>
+        </a>
       </div>
-      <div className="user-repo-item-right">
-        <div className="user-repo-item-stats">
-          <p className="user-repo-item-stats-count" id="count">
-            {repo.stargazers_count}
-          </p>
-          <label htmlFor="count" className="user-repo-item-stats-count-label">
-            Stars
-          </label>
-        </div>
-      </div>
-    </a>
+    </>
   );
 };
 
